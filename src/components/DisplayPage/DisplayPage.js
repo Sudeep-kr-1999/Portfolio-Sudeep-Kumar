489cad309c576a247372b75f31f1d7446bf91174
Sudeep Kumar <sudeepagrawal1999@gmail.com>
2021-12-27 16:56:56 +0530

    Functionality Added for buttons and links

--- src/components/DisplayPage/DisplayPage.js
+++ src/components/DisplayPage/DisplayPage.js
@@ -1,16 +1,37 @@
 import React from "react";
 import "../DisplayPage/displaypage.css";
+import DpButtonLinks from "../DpButtonLinks/DpButtonLinks";
 import background from "./assets/background10.jpg";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
 function DisplayPage() {
   const name = "Sudeep Kumar Agrawal";
   const specification = "Student and Developer";
+  const buttonType = [
+    { name: "Github", id: 1, component: <GitHubIcon /> },
+    { name: "Linkedin", id: 2, component: <LinkedInIcon /> },
+    { name: "Facebook", id: 3, component: <FacebookRoundedIcon /> },
+    { name: "Instagram", id: 4, component: <InstagramIcon /> },
+    { name: "Twitter", id: 5, component: <TwitterIcon /> },
+  ];
   return (
     <div className="display-page-body">
       <img src={background} alt="background" className="display-bg" />
       <div className="display-page-content">
         <div className="display-name">{name}</div>
         <div className="display-specification">{specification}</div>
-        <div className="display-buttons">buttons</div>
+        <div className="display-buttons">
+          {buttonType.map((buttonObj) => (
+            <DpButtonLinks
+              key={buttonObj.id}
+              buttonName={buttonObj.name}
+              component={buttonObj.component}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/DpButtonLinks/DpButtonLinks.js
+++ src/components/DpButtonLinks/DpButtonLinks.js
@@ -1,40 +1,34 @@
-import React from "react";
-import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import GitHubIcon from "@mui/icons-material/GitHub";
+import React, { useState } from "react";
 import "./dpbuttonlinks.css";
-function DpButtonLinks({ classname }) {
+
+function DpButtonLinks(props) {
+  const Facebook = "https://www.facebook.com/your.sudeepkumar";
+  const Twitter = "https://twitter.com/_kumar_sudeep_";
+  const Instagram = "https://www.instagram.com/sudeep.kumar.official/";
+  const Github = "https://github.com/Sudeep-kr-1999";
+  const Linkedin = "https://www.linkedin.com/in/sudeep-kumar-78416b191/";
+
+  const [buttonType, setbuttonType] = useState("");
+  const openLink = (e) => {
+    const button = e.target.id;
+    setbuttonType(button);
+  };
+  if (buttonType === "Facebook") {
+    window.location.href = Facebook;
+  } else if (buttonType === "Twitter") {
+    window.location.href = Twitter;
+  } else if (buttonType === "Instagram") {
+    window.location.href = Instagram;
+  } else if (buttonType === "Github") {
+    window.location.href = Github;
+  } else if (buttonType === "Linkedin") {
+    window.location.href = Linkedin;
+  }
   return (
-    <div>
-      {classname === "facebook-button" && (
-        <div className={classname}>
-          <FacebookRoundedIcon fontSize="large" />
-        </div>
-      )}
-      {classname === "twitter-button" && (
-        <div className={classname}>
-          <TwitterIcon fontSize="large" />
-        </div>
-      )}
-      {classname === "instagram-button" && (
-        <div className={classname}>
-          <LinkedInIcon fontSize="large" />
-        </div>
-      )}
-      {classname === "linkedin-button" && (
-        <div className={classname}>
-          <InstagramIcon fontSize="large" />
-        </div>
-      )}
-      {classname === "github-button" && (
-        <div className={classname}>
-          <GitHubIcon fontSize="large" />
-        </div>
-      )}
+    <div className="button-icon" id={`${props.buttonName}`} onClick={openLink}>
+      {props.component}
+      {props.buttonName}
     </div>
   );
 }
-
 export default DpButtonLinks;
