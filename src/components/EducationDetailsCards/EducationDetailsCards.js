8f1d5c3220c32aa734e29955aef371e4546c679f
Sudeep Kumar <sudeepagrawal1999@gmail.com>
2021-12-29 14:23:21 +0530

    Education details ui changed and animation added

--- src/components/EducationDetailsCards/EducationDetailsCards.js
+++ src/components/EducationDetailsCards/EducationDetailsCards.js
@@ -5,9 +5,6 @@ function EducationDetailsCards(props) {
   return (
     <div className="education-cards">
       <Fade bottom duration={2000} delay={1000}>
-        <div className="card-image">
-          <img src={props.logo} alt="logo" />
-        </div>
         <div className="education-standard">{props.educationType}</div>
         <div className="education-subheading">{props.educationSubDetail}</div>
         <div className="organisation-name">{props.organisationName}</div>
